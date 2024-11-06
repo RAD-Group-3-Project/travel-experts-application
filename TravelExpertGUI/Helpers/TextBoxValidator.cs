a1d7c97a85db183f74930d9a7c61f7494e0b64e8
sorawt38 <st.sorawat38@gmail.com>
2024-11-06 00:19:36 -0700

    [feat] add IsPresent() method

---
+++ TravelExpertGUI/Helpers/TextBoxValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertGUI.Helpers;
+public static class TextBoxValidator
+{
+    public static bool IsPresent(TextBox textBox)
+    {
+        if (string.IsNullOrWhiteSpace(textBox.Text))
+        {
+            MessageBox.Show($"Please enter a value for {textBox.Tag}.", "Input Required");
+            textBox.SelectAll();
+            textBox.Focus();
+            return false;
+        }
+
+        return true;
+    }
+}
