a8d536bc22602cd53cc148490b712950f1afa9ab
sorawt38 <st.sorawat38@gmail.com>
2024-11-11 16:03:40 -0700

    [feat] init empty validator util project

---
+++ TravelExpertUtils/Validator.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelExpertUtils;
+/**************************************************************************
+ * Filename:        validator.cs
+ * Author:          Sorawat (James) Tanthikun
+ * Created Date:    2024-11-11
+ *
+ * Description:
+ *     For normal validation operation that not involve with TextBox
+ *
+ **************************************************************************/
+
+public class Validator
+{
+
+}
