f71587de9c45c10292513da3e67185b2e7824f47
Chris <162272458+Crutchface@users.noreply.github.com>
2024-11-13 11:42:14 -0700

    Update database_update.sql
    Added suppliers table updates.

--- TravelExpertData/Scripts/database_update.sql
+++ TravelExpertData/Scripts/database_update.sql
@@ -17,3 +17,11 @@ ALTER TABLE Agencies
 
 ALTER TABLE Agents
     ADD is_active BIT NOT NULL DEFAULT 1;
+
+
+-- Nov 13
+alter table Suppliers add 
+is_active bit default 1
+
+UPDATE Suppliers
+SET is_active = 1;
