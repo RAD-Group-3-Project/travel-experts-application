7912fdc017f633e0f7ad877daf7c8567f9b6bd30
sorawt38 <st.sorawat38@gmail.com>
2024-11-12 20:07:55 -0700

    [build] do scaffold again

--- TravelExpertData/Models/PackagesProductsSupplier.cs
+++ TravelExpertData/Models/PackagesProductsSupplier.cs
@@ -10,7 +10,7 @@ namespace TravelExpertData.Models;
 [Index("PackageId", Name = "PackagesPackages_Products_Suppliers")]
 [Index("ProductSupplierId", Name = "ProductSupplierId")]
 [Index("ProductSupplierId", Name = "Products_SuppliersPackages_Products_Suppliers")]
-[Index("PackageId", "ProductSupplierId", Name = "UQ__Packages__29CA8E95B83A774E", IsUnique = true)]
+[Index("PackageId", "ProductSupplierId", Name = "UQ__Packages__29CA8E95D9D6058F", IsUnique = true)]
 public partial class PackagesProductsSupplier
 {
     [Key]
