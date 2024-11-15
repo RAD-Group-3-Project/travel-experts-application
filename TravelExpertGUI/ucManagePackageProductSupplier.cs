a924e85bc378e12415cc3695ed6d66c2d06aacca
Chris <chris.dg.ferguson@gmail.com>
2024-11-15 10:00:46 -0700

    Added comments PPS

--- TravelExpertGUI/ucManagePackageProductSupplier.cs
+++ TravelExpertGUI/ucManagePackageProductSupplier.cs
@@ -11,34 +11,35 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TravelExpertData.Models;
 using TravelExpertData.Repositories;
+using TravelExpertGUI.Helpers;
 
 namespace TravelExpertGUI
 {
     public partial class ucManagePackageProductSupplier : UserControl
     {
-        //private int selectedPackageProductSupplierID;
-        //private int selectedPackageID;
-        //private int selectedProductSupplierID;
+        
         private bool isAddition;
 
         public ucManagePackageProductSupplier()
         {
             InitializeComponent();
         }
-
+        // Populates our list with active PPS
         private void ucManagePackageProductSupplier_Load(object sender, EventArgs e)
         {
             populatePPS();
 
         }
-
+        // Function to poulate our DGV
         private void populatePPS()
-        {
+        {   
+            // Makes it clickable
             dgvPackageProductSupplier.Enabled = true;
             // Sets textboxes and buttons to appropriate status
             txtPackageProductSupplierId.ReadOnly = true;
             cmbPackageID.Enabled = false;
             cmbProductSupplierID.Enabled = false;
+            // Activates and deactivates appropriate buttons
             btnAdd.Enabled = true;
             btnDelete.Enabled = true;
             btnEdit.Enabled = true;
@@ -54,27 +55,35 @@ namespace TravelExpertGUI
 
 
         }
-
+        // Gets our current row and changes selected PPS
         private void dgvPackageProductSupplier_SelectionChanged(object sender, EventArgs e)
-        {
+        {   // If a row is actually selected 
             if (dgvPackageProductSupplier.CurrentRow != null) // if row selected
             {
-                loadPackageID();
+                try
+                {
+                    // Populates our dropdown boxes 
+                    loadPackageID();
+                    // Changes our cmb boxes and textboxes values to match what is selected
+                    txtPackageProductSupplierId.Text = dgvPackageProductSupplier.CurrentRow.Cells["PackageProductSupplierId"].Value.ToString();
+                    int selectedPackageId = Convert.ToInt32(dgvPackageProductSupplier.CurrentRow.Cells["PackageId"].Value);
+                    int selectedSupplierId = Convert.ToInt32(dgvPackageProductSupplier.CurrentRow.Cells["ProductSupplierId"].Value);
+                    // Selects cmb boxes 
+                    cmbPackageID.SelectedValue = selectedPackageId;
+                    cmbProductSupplierID.SelectedValue = selectedSupplierId;
+                }
+                catch (Exception ex)
+                {
 
-                txtPackageProductSupplierId.Text = dgvPackageProductSupplier.CurrentRow.Cells["PackageProductSupplierId"].Value.ToString();
-
-                int selectedPackageId = Convert.ToInt32(dgvPackageProductSupplier.CurrentRow.Cells["PackageId"].Value);
-                int selectedSupplierId = Convert.ToInt32(dgvPackageProductSupplier.CurrentRow.Cells["ProductSupplierId"].Value);
-
-                cmbPackageID.SelectedValue = selectedPackageId;
-                cmbProductSupplierID.SelectedValue = selectedSupplierId;
+                    MessageBox.Show($"Error: {ex.Message}", "PPS Read Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
             }
         }
-
+        // Gets 2 list and populates combo boxes with them 
         private void loadPackageID()
         {
-
+            // gets pur lists
             List<Package> package = PackageRepository.GetPackages();
             List<ProductsSupplier> supplier = ProductSuppliersRepository.GetProductSuppliers();
 
@@ -86,89 +95,129 @@ namespace TravelExpertGUI
             cmbProductSupplierID.ValueMember = "ProductSupplierId";
             cmbProductSupplierID.DisplayMember = "ProductSupplierId";
         }
-
+        // Start of Add Function
         private void btnAdd_Click(object sender, EventArgs e)
-        {
+        {   
+            // Makes our dgv inactive and allows us to enter new values 
             dgvPackageProductSupplier.Enabled = false;
+            // Resets combo boxes 
             cmbPackageID.SelectedIndex = -1;
             cmbProductSupplierID.SelectedIndex = -1;
+            // Clears boxes 
             txtPackageProductSupplierId.Clear();
             txtPackageProductSupplierId.ReadOnly = true;
+            // Enables dropdown
             cmbPackageID.Enabled = true;
             cmbProductSupplierID.Enabled = true;
+            // Sets buttons 
             btnAdd.Enabled = false;
             btnDelete.Enabled = false;
             btnEdit.Enabled = false;
             btnDisc.Enabled = true;
             btnSave.Enabled = true;
+            // Changes function
             isAddition = true;
         }
-
+        // Saves our changes 
         private void btnSave_Click(object sender, EventArgs e)
-        {
+        {   
+            // Checks function 
             if (isAddition)
-            {
-                PackagesProductsSupplier addedPPS = new PackagesProductsSupplier();
-                addedPPS.PackageId = Int32.Parse(cmbPackageID.SelectedValue.ToString());
-                addedPPS.ProductSupplierId = Int32.Parse(cmbProductSupplierID.SelectedValue.ToString());
-                addedPPS.IsActive = true;
-                PackagesProductsSuppliersRepository.CreatePPS(addedPPS);
-                populatePPS();
-
-            }
-            else
-            {
-                PackagesProductsSupplier updatedPPS = new PackagesProductsSupplier();
-                updatedPPS.PackageProductSupplierId = Int32.Parse(txtPackageProductSupplierId.Text.ToString());
-                updatedPPS.PackageId = Int32.Parse(cmbPackageID.SelectedValue.ToString());
-                updatedPPS.ProductSupplierId = Int32.Parse(cmbProductSupplierID.SelectedValue.ToString());
-                updatedPPS.IsActive = true;
-                PackagesProductsSuppliersRepository.UpdatePPS(updatedPPS);
-                populatePPS();
-            }
-        }
-
-        private void btnEdit_Click(object sender, EventArgs e)
-        {
-            dgvPackageProductSupplier.Enabled = false;
-            txtPackageProductSupplierId.ReadOnly = true;
-            cmbPackageID.Enabled = true;
-            cmbProductSupplierID.Enabled = true;
-            btnAdd.Enabled = false;
-            btnDelete.Enabled = false;
-            btnEdit.Enabled = false;
-            btnDisc.Enabled = true;
-            btnSave.Enabled = true;
-            isAddition = false;
-        }
-
-        private void btnDisc_Click(object sender, EventArgs e)
-        {
-            populatePPS();
-        }
-
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            DataGridViewRow selectedRow = dgvPackageProductSupplier.CurrentRow;
-            int selectedPPS = Convert.ToInt32(selectedRow.Cells[0].Value);
-            DialogResult result =
-           MessageBox.Show($"Are you sure you would like to delete PPS #{selectedRow.Cells[0].Value} ",
-           "Confirm Agent Deletion",
-           MessageBoxButtons.YesNo,
-           MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
             {
                 try
                 {
-                    PackagesProductsSuppliersRepository.DeletePPS(selectedPPS);
-                    populatePPS();
+                    // Makes new PPS and gets info
+                    PackagesProductsSupplier addedPPS = new PackagesProductsSupplier();
+                    addedPPS.PackageId = Int32.Parse(cmbPackageID.SelectedValue.ToString());
+                    addedPPS.ProductSupplierId = Int32.Parse(cmbProductSupplierID.SelectedValue.ToString());
+                    addedPPS.IsActive = true;
+                    PackagesProductsSuppliersRepository.CreatePPS(addedPPS);
                 }
                 catch (Exception ex)
                 {
 
-                    MessageBox.Show($"Error: {ex.Message}", "PPS Delete Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show($"Error: {ex.Message}", "PPS Add Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
+                // Resets the list
+                populatePPS();
+
             }
+            // Otherwise we are updating
+            else
+            {
+                try
+                {
+                    // New Pps 
+                    PackagesProductsSupplier updatedPPS = new PackagesProductsSupplier();
+                    // Gets our info
+                    if (TextBoxValidator.IsPresent(txtPackageProductSupplierId) && TextBoxValidator.IsInteger(txtPackageProductSupplierId))
+                    {
+                        updatedPPS.PackageProductSupplierId = Int32.Parse(txtPackageProductSupplierId.Text.ToString());
+                    }
+                    updatedPPS.PackageId = Int32.Parse(cmbPackageID.SelectedValue.ToString());
+                    updatedPPS.ProductSupplierId = Int32.Parse(cmbProductSupplierID.SelectedValue.ToString());
+                    updatedPPS.IsActive = true;
+                    // Updates 
+                    PackagesProductsSuppliersRepository.UpdatePPS(updatedPPS);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Error: {ex.Message}", "PPS Edit Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                // Resets List
+                populatePPS();
+            }
+        }
+        // Our Edit Function
+        private void btnEdit_Click(object sender, EventArgs e)
+        {   
+            // Makes appropriate buttons and boxes available
+            dgvPackageProductSupplier.Enabled = false;
+            txtPackageProductSupplierId.ReadOnly = true;
+            cmbPackageID.Enabled = true;
+            cmbProductSupplierID.Enabled = true;
+            btnAdd.Enabled = false;
+            btnDelete.Enabled = false;
+            btnEdit.Enabled = false;
+            btnDisc.Enabled = true;
+            btnSave.Enabled = true;
+            // Changes our function
+            isAddition = false;
+        }
+        // Discard Button
+        private void btnDisc_Click(object sender, EventArgs e)
+        {   
+            // Resets form
+            populatePPS();
+        }
+        // Delete Function
+        private void btnDelete_Click(object sender, EventArgs e)
+        {   
+            // Gets our selected row
+            DataGridViewRow selectedRow = dgvPackageProductSupplier.CurrentRow;
+            // Saves an int of the id 
+            int selectedPPS = Convert.ToInt32(selectedRow.Cells[0].Value);
+            // Popup for delete
+            DialogResult result =
+            MessageBox.Show($"Are you sure you would like to delete PPS #{selectedRow.Cells[0].Value} ",
+            "Confirm Agent Deletion",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question);
+            // If yes delete
+            if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        PackagesProductsSuppliersRepository.DeletePPS(selectedPPS);
+                        populatePPS();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show($"Error: {ex.Message}", "PPS Delete Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
 
         }
     }
--- TravelExpertGUI/ucManageProductSuppliers.Designer.cs
+++ TravelExpertGUI/ucManageProductSuppliers.Designer.cs
@@ -112,7 +112,7 @@ partial class ucManageProductSuppliers
         // txtProductSupplierId
         // 
         txtProductSupplierId.BackColor = SystemColors.Control;
-        txtProductSupplierId.Location = new Point(178, 45);
+        txtProductSupplierId.Location = new Point(212, 57);
         txtProductSupplierId.Name = "txtProductSupplierId";
         txtProductSupplierId.Size = new Size(234, 27);
         txtProductSupplierId.TabIndex = 49;
@@ -120,7 +120,7 @@ partial class ucManageProductSuppliers
         // label2
         // 
         label2.AutoSize = true;
-        label2.Location = new Point(67, 116);
+        label2.Location = new Point(67, 113);
         label2.Name = "label2";
         label2.Size = new Size(107, 20);
         label2.TabIndex = 50;
@@ -129,7 +129,7 @@ partial class ucManageProductSuppliers
         // label3
         // 
         label3.AutoSize = true;
-        label3.Location = new Point(67, 176);
+        label3.Location = new Point(67, 156);
         label3.Name = "label3";
         label3.Size = new Size(111, 20);
         label3.TabIndex = 52;
@@ -139,29 +139,30 @@ partial class ucManageProductSuppliers
         // 
         cboProductName.BackColor = SystemColors.Control;
         cboProductName.FormattingEnabled = true;
-        cboProductName.Location = new Point(212, 113);
+        cboProductName.Location = new Point(212, 105);
         cboProductName.Margin = new Padding(3, 4, 3, 4);
         cboProductName.Name = "cboProductName";
-        cboProductName.Size = new Size(100, 23);
+        cboProductName.Size = new Size(100, 28);
         cboProductName.TabIndex = 54;
         // 
         // cboSupplierName
         // 
         cboSupplierName.BackColor = SystemColors.Control;
         cboSupplierName.FormattingEnabled = true;
-        cboSupplierName.Location = new Point(212, 173);
+        cboSupplierName.Location = new Point(212, 153);
         cboSupplierName.Margin = new Padding(3, 4, 3, 4);
         cboSupplierName.Name = "cboSupplierName";
-        cboSupplierName.Size = new Size(100, 23);
+        cboSupplierName.Size = new Size(100, 28);
         cboSupplierName.TabIndex = 55;
         // 
         // dgvProductSupplier
         // 
         dgvProductSupplier.BackgroundColor = SystemColors.GradientActiveCaption;
         dgvProductSupplier.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dgvProductSupplier.Location = new Point(48, 179);
+        dgvProductSupplier.Location = new Point(48, 205);
         dgvProductSupplier.Name = "dgvProductSupplier";
-        dgvProductSupplier.Size = new Size(704, 175);
+        dgvProductSupplier.RowHeadersWidth = 51;
+        dgvProductSupplier.Size = new Size(704, 149);
         dgvProductSupplier.TabIndex = 56;
         dgvProductSupplier.SelectionChanged += dgvProductSupplier_SelectionChanged;
         // 
